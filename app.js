ff0a1c44962288f5a7c29c3427587d4b45003a09
nicatzamanwork <webdevelopernicat@gmail.com>
2023-02-04 18:28:56 +0400

    first

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// conenction to mongodb
+mongoose.connect(
+  "mongodb+srv://nijatzaman:1234_Jerusalem@cluster0.loje51j.mongodb.net/ToDo",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+// routes
+app.use(require("./routes/index"));
+app.use(require("./routes/todo"));
+
+// server configurations....
+app.listen(3000, () => console.log("Server started listening on port: 3000"));
